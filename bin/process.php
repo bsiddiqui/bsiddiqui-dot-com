807e734a51045f45878f7d0097cece27d17c7e9d
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-05-14 00:36:26 -0400

    restructured, html validates

--- bin/process.php
+++ bin/process.php
@@ -1,12 +1,18 @@
-<? php
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+<?php
+if ((isset($_POST['name'])) && (strlen(trim($_POST['name'])) > 0)) {
+	$name = stripslashes(strip_tags($_POST['name']));
+} else {$name = 'No name entered';}
+if ((isset($_POST['email'])) && (strlen(trim($_POST['email'])) > 0)) {
+	$email = stripslashes(strip_tags($_POST['email']));
+} else {$email = 'No email entered';}
+if ((isset($_POST['message'])) && (strlen(trim($_POST['message'])) > 0)) {
+	$message = stripslashes(strip_tags($_POST['message']));
+} else {$message = 'No message entered';}
 
-    $to      = "basil.siddiqui@gmail.com";
-    $subject = "Someone contacted you!"
-    $message = $name . " | " . $email . "\n\n" . $message;
-    $headers = "From: bsiddiqu.com";
+$to      = "basil.siddiqui@gmail.com";
+$subject = "A message for you! :)";
+$message = $name . " | " . $email . "\n\n" . $message;
+$headers = "From: basil.siddiqui@gmail.com";
 
-    mail($to, $subject, $message, $headers);
+mail($to, $subject, $message, $headers);
 ?>
